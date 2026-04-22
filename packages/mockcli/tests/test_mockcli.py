010e5cefa929c1677b6ed6e0fbacdc2cb4657136
oyghen <oyghen@duck.com>
2026-04-22 06:04:06 +0200

    test: remove constants

--- packages/mockcli/tests/test_mockcli.py
+++ packages/mockcli/tests/test_mockcli.py
@@ -7,12 +7,10 @@ from mocklib.core import hello
 
 def test_project_name(project_name: str, project_pkg: ModuleType):
     assert project_pkg.__name__ == project_name
-    assert project_pkg.__name__ == "mockcli"
 
 
 def test_project_version(project_name: str, project_pkg: ModuleType):
     assert project_pkg.__version__ == metadata.version(project_name)
-    assert project_pkg.__version__ == "0.3.0"
 
 
 def test_mocklib_name():
--- packages/mocklib/tests/test_mocklib.py
+++ packages/mocklib/tests/test_mocklib.py
@@ -4,9 +4,7 @@ from types import ModuleType
 
 def test_project_name(project_name: str, project_pkg: ModuleType):
     assert project_pkg.__name__ == project_name
-    assert project_pkg.__name__ == "mocklib"
 
 
 def test_project_version(project_name: str, project_pkg: ModuleType):
     assert project_pkg.__version__ == metadata.version(project_name)
-    assert project_pkg.__version__ == "0.3.0"
